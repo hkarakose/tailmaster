b0fb31b8924a004ef75765e11aedd7656b19e4be
halilkarakose <halilkarakose@5e555e78-e547-11dd-bd7b-b12b6590754f>
2009-01-22 17:44:20 +0000

    add class containing contant values

---
+++ src/main/java/tailmaster/util/Constants.java
@@ -0,0 +1,11 @@
+package tailmaster.util;
+
+/**
+ * User: Halil KARAKOSE
+ * Date: Jan 20, 2009
+ * Time: 10:46:35 PM
+ */
+public class Constants {
+	public static final String TAIL_THREADS = "TAIL_THREADS";
+	public static final String PROGRAM_NAME = "tailmaster";
+}
