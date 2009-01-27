373c114f534d8861cda253edc7b17c8d170588b2
halilkarakose <halilkarakose@5e555e78-e547-11dd-bd7b-b12b6590754f>
2009-01-27 19:47:46 +0000

    added icon to JFrame

--- src/main/java/tailmaster/gui/TailMasterFrame.java
+++ src/main/java/tailmaster/gui/TailMasterFrame.java
@@ -4,6 +4,10 @@ import tailmaster.gui.menu.TailMasterMenuBar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.File;
 
 /**
  * User: Halil KARAKOSE
@@ -11,8 +15,8 @@ import java.awt.*;
  * Time: 4:35:13 PM
  */
 public class TailMasterFrame extends JFrame {
-    private JPanel tabbedPaneContainer;
-    private CloseButtonTabbedPane tabbedPane;
+	private JPanel tabbedPaneContainer;
+	private CloseButtonTabbedPane tabbedPane;
 	private static TailMasterFrame instance;
 
 	public static TailMasterFrame getInstance() {
@@ -21,21 +25,45 @@ public class TailMasterFrame extends JFrame {
 	}
 
 	private TailMasterFrame() throws HeadlessException {
+		JFrame.setDefaultLookAndFeelDecorated(false);
+		setUndecorated(false);
 		setTitle("TailMaster");
 		setLayout(new BorderLayout());
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setJMenuBar(new TailMasterMenuBar());
+		Image image = new ImageIcon(getClass().getClassLoader().getResource("armour.png")).getImage();
+		setIconImage(image);
 
-        tabbedPane = new CloseButtonTabbedPane();
+		setJMenuBar(new TailMasterMenuBar());
+
+		tabbedPane = new CloseButtonTabbedPane();
 		tabbedPaneContainer = new JPanel(new BorderLayout());
-        tabbedPaneContainer.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
-        tabbedPaneContainer.add(tabbedPane, SwingConstants.CENTER);
-        add(tabbedPaneContainer, SwingConstants.CENTER);
+		tabbedPaneContainer.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		tabbedPaneContainer.add(tabbedPane, SwingConstants.CENTER);
+		add(tabbedPaneContainer, SwingConstants.CENTER);
 	}
 
 	public CloseButtonTabbedPane getTabbedPane() {
-        return tabbedPane;
-    }
+		return tabbedPane;
+	}
+	protected static Image createFDImage() {
+		//Create a 16x16 pixel image.
+		BufferedImage bi = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
+
+		//Draw into it.
+		Graphics g = bi.getGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 15, 15);
+		g.setColor(Color.RED);
+		g.fillOval(5, 3, 6, 6);
+
+		//Clean up.
+		g.dispose();
+
+		//Return it.
+		return bi;
+	}
+
 }
+
