bfad888a9945e71b5bcd86bc6b8f148a47c515ed
halilkarakose <halilkarakose@5e555e78-e547-11dd-bd7b-b12b6590754f>
2009-02-27 20:10:49 +0000

    removed unused code

--- src/main/java/tailmaster/gui/TailMasterFrame.java
+++ src/main/java/tailmaster/gui/TailMasterFrame.java
@@ -53,23 +53,5 @@ public class TailMasterFrame extends JFrame {
     public CloseButtonTabbedPane getTabbedPane() {
 		return tabbedPane;
 	}
-	protected static Image createFDImage() {
-		//Create a 16x16 pixel image.
-		BufferedImage bi = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
-
-		//Draw into it.
-		Graphics g = bi.getGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, 15, 15);
-		g.setColor(Color.RED);
-		g.fillOval(5, 3, 6, 6);
-
-		//Clean up.
-		g.dispose();
-
-		//Return it.
-		return bi;
-	}
-
 }
 
