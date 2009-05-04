47a6202631b736029ed496827a0ebd9da6002c08
halilkarakose <halilkarakose@5e555e78-e547-11dd-bd7b-b12b6590754f>
2009-05-04 18:05:52 +0000

    display panel refactor to support tail panels and ssh terminal panels

--- src/main/java/tailmaster/gui/LogDisplayPanel.java
+++ src/main/java/tailmaster/gui/LogDisplayPanel.java
@@ -13,85 +13,11 @@ import java.awt.event.*;
  * Date: 14.01.2009
  * Time: 12:17:21
  */
-public class LogDisplayPanel extends JPanel {
-    private JTextArea logTextArea;
-    private JScrollPane scroller;
-    private long panelId;
+public class LogDisplayPanel extends TailMasterPanel {
+	//TODO tailmasterpanel yaz ve bu class'i tailmasterpanel'den extend et.
 
     public LogDisplayPanel() {
-        setLayout(new BorderLayout());
-        setBorder(new EmptyBorder(1, 1, 1, 1));
-        setPanelId(System.currentTimeMillis());
-        TabRegistry.INSTANCE.addTab(getPanelId(), new TabData(panelId));
-
-        addComponentListener(new PanelComponentListener());
-
-        logTextArea = new JTextArea();
-        logTextArea.setEditable(false);
-        logTextArea.setFont(new Font("Courier New", Font.PLAIN, 12));
-		logTextArea.setLineWrap(true);
-		logTextArea.setWrapStyleWord(true);
-		logTextArea.getCaret().setVisible(true);
-        logTextArea.addMouseListener(new LogTextAreaMouseMotionListener());
-
-        scroller = new JScrollPane(logTextArea);
-        scroller.setAutoscrolls(true);
-        add(scroller);
+		textArea = new TailMasterTextArea();
+		scroller.setViewportView(textArea);
     }
-
-    public JTextArea getLogTextArea() {
-        return logTextArea;
-    }
-
-    public JScrollPane getScroller() {
-        return scroller;
-    }
-
-    public long getPanelId() {
-        return panelId;
-    }
-
-    public void setPanelId(long panelId) {
-        this.panelId = panelId;
-    }
-
-    private static class LogTextAreaMouseMotionListener extends MouseAdapter {
-
-        public void mouseMoved(MouseEvent e) {
-            //http://java.sun.com/j2se/1.4.2/docs/api/java/awt/Cursor.html
-            //http://java.sun.com/docs/books/tutorial/uiswing/events/mouselistener.html
-            //http://java.sun.com/javase/6/docs/api/java/awt/event/MouseListener.html#mouseEntered(java.awt.event.MouseEvent)
-        }
-
-
-        public void mouseEntered(MouseEvent e) {
-            JTextArea textArea = (JTextArea) e.getComponent();
-            textArea.setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
-        }
-
-        public void mouseExited(MouseEvent e) {
-        }
-    }
-
-	private static class PanelComponentListener implements ComponentListener {
-		@Override
-            public void componentResized(ComponentEvent e) {
-		}
-
-		@Override
-            public void componentMoved(ComponentEvent e) {
-		}
-
-		@Override
-            public void componentShown(ComponentEvent e) {
-			LogDisplayPanel panel = (LogDisplayPanel) e.getComponent();
-			boolean playing = TabRegistry.INSTANCE.getTabData(panel.getPanelId()).isPlaying();
-			TailMasterToolBar toolBar = (TailMasterToolBar) TailMasterFrame.getInstance().getToolBar();
-			toolBar.togglePauseButtonText(playing);
-		}
-
-		@Override
-            public void componentHidden(ComponentEvent e) {
-		}
-	}
 }
--- src/main/java/tailmaster/gui/TerminalPanel.java
+++ src/main/java/tailmaster/gui/TerminalPanel.java
@@ -4,45 +4,49 @@ import com.sshtools.j2ssh.session.SessionChannelClient;
 import com.sshtools.j2ssh.connection.ChannelOutputStream;
 import com.sshtools.j2ssh.connection.ChannelInputStream;
 
-import javax.swing.*;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
 import java.io.*;
 
 import tailmaster.sshterm.EscapeCharacterManager;
+import tailmaster.commons.gui.BlockCaret;
 
 /**
  * User: Halil KARAKOSE
  * Date: Feb 6, 2009
  * Time: 12:33:36 AM
  */
-public class TerminalPanel extends LogDisplayPanel {
+public class TerminalPanel extends TailMasterPanel {
 	private ChannelOutputStream outputStream;
 	private ChannelInputStream inputStream;
     private EscapeCharacterManager escapeCharacterManager;
 
 	public TerminalPanel(SessionChannelClient channel) {
-		super();
         this.escapeCharacterManager = new EscapeCharacterManager();
 		this.outputStream = channel.getOutputStream();
 
 		this.inputStream = channel.getInputStream();
 
-		final JTextArea textArea = getLogTextArea();
-		getLogTextArea().addKeyListener(new TerminalKeyListener());
-
+		textArea = new TailMasterTextArea();
+		textArea.addKeyListener(new TerminalKeyListener());
+		textArea.setCaret(new BlockCaret());
+		scroller.setViewportView(textArea);
 		new Thread() {
 			public void run() {
 				char charCode;
 				try {
+                    //TODO convert to timer task
 					while (true) {
 						if ((charCode = (char) inputStream.read()) != -1) {
-                            System.out.println((short) charCode + "-" + charCode);
-                            if (escapeCharacterManager.canWrite(charCode)) {
-                                textArea.append(String.valueOf(charCode));
-                                textArea.setCaretPosition(textArea.getText().length());
+                            if (exitTerminal(charCode)) {
+                                //TODO disconnect
+                                System.out.println("terminate session");
+                                break;
                             }
-						} else {
+
+                            processInput(charCode, textArea);
+                        } else {
 							Thread.sleep(2000);
 						}
 					}
@@ -56,8 +60,19 @@ public class TerminalPanel extends LogDisplayPanel {
 
 		textArea.requestFocus();
 	}
-	
-	private class TerminalKeyListener implements KeyListener {
+
+    private boolean exitTerminal(char charCode) {
+        return (charCode == Character.MAX_VALUE);
+    }
+
+    private void processInput(char charCode, TailMasterTextArea terminalTextArea) {
+        if (escapeCharacterManager.canWrite(charCode)) {
+            terminalTextArea.append(String.valueOf(charCode));
+            terminalTextArea.setCaretPosition(terminalTextArea.getText().length());
+        }
+    }
+
+    private class TerminalKeyListener extends KeyAdapter {
 		public void keyTyped(KeyEvent e) {
 			char keyChar = e.getKeyChar();
 			try {
@@ -67,14 +82,5 @@ public class TerminalPanel extends LogDisplayPanel {
 				e1.printStackTrace();
 			}
 		}
-
-		public void keyPressed(KeyEvent e) {
-			//TODO check for UP DOWN
-		}
-
-		public void keyReleased(KeyEvent e) {
-		}
 	}
-
-
 }
